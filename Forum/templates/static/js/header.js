d1325761eeae8c4da74415015a3d9c919c613f07
Dias1c <dias.kappasov@gmail.com>
2021-12-06 21:00:21 +0600

    search finished + added hint

--- Forum/templates/static/js/header.js
+++ Forum/templates/static/js/header.js
@@ -1,1 +1,17 @@
 // Widgets like Search
+var cb_Search = document.getElementById("cb_search");
+var b_Search = document.getElementById("b_search");
+
+// cb_Search_change - Shows or hide element
+function cb_Search_change(e) {
+    if (e.target.checked) {
+        b_Search.classList.add("dflex-on-md");
+        cb_Search.classList.add("search-btn--checked");
+    } else {
+        b_Search.classList.remove("dflex-on-md");
+        cb_Search.classList.remove("search-btn--checked");
+    }
+}
+
+// ADDING EVENTS
+cb_Search.addEventListener("change", cb_Search_change);
